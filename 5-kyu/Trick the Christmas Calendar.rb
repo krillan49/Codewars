9ec76fb5435beda240435fb81b7484f314294823
krillan <pushkinidze49@gmail.com>
2023-07-16 13:09:30 +0300

    5-kyu/Trick the Christmas Calendar

---
+++ 5-kyu/Trick the Christmas Calendar.rb
@@ -0,0 +1,22 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# It's almost Christmas and you have a pretty cool Christmas Calendar with tasty sweets and awesome toys in it. As curious as you are, you want to open all the doors. But unfortunately only the today's door can be opened. Can you find a way to open the 24th door already? :)
+#
+# PS: Yes, if you do this kata on December 24th, you'll get a solved kata for free. But the other 364 days of the year, you have to trick a bit.
+#
+# Good Luck and Merry X-Mas!
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+class Date
+  def month
+    12
+  end
+  def day
+    24
+  end
+end
