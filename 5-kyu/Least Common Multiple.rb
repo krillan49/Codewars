78fe82d5d478ded8fa68bba014e3bdb081c36ecd
krillan <pushkinidze49@gmail.com>
2023-08-08 13:07:19 +0300

    5-kyu/Least Common Multiple

---
+++ 5-kyu/Least Common Multiple.rb
@@ -0,0 +1,13 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# Write a function that calculates the least common multiple of its arguments; each argument is assumed to be a non-negative integer. In the case that there are no arguments (or the provided array in compiled languages is empty), return 1. If any argument is 0, return 0.
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+def lcm(*a)
+  a.inject(:lcm)
+end
