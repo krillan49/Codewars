fb5663c8383f0d82e50a555f473951687ecb1438
krillan <pushkinidze49@gmail.com>
2023-08-22 15:03:04 +0300

    5-kyu/Not very secure

---
+++ 5-kyu/Not very secure.rb
@@ -0,0 +1,19 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+#
+# The string has the following conditions to be alphanumeric:
+#
+# At least one character ("" is not valid)
+# Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+# No whitespaces / underscore
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+def alphanumeric?(string)
+  string.match?(/\A[a-z\d]+$/i)
+end
