9ee4cd32c86161426c007dc97db678e9902bc604
krillan <pushkinidze49@gmail.com>
2023-08-24 14:20:57 +0300

    5-kyu/Extract the domain name from a URL

---
+++ 5-kyu/Extract the domain name from a URL.rb
@@ -0,0 +1,17 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+#
+# * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+# * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+# * url = "https://www.cnet.com"                -> domain name = cnet"
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+def domain_name(url)
+  url.gsub(/^(.*\/\/)*(www\.)*([^\.]+).+$/, '\3')
+end
