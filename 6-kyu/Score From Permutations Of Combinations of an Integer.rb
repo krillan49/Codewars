cf533266b93ddc57db35499a9724a3faf78ece8b
krillan <pushkinidze49@gmail.com>
2023-11-05 14:38:26 +0300

    6-kyu/Score From Permutations Of Combinations of an Integer

---
+++ 6-kyu/Score From Permutations Of Combinations of an Integer.rb
@@ -0,0 +1,27 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# We define the score of permutations of combinations, of an integer number (the function to obtain this value:sc_perm_comb) as the total sum of all the numbers obtained from the permutations of all the possible combinations of its digits. For example we have the number 348.
+#
+# sc_perm_comb(348) = 3 + 4 + 8 + 34 + 38 + 48 + 43 + 83 + 84 + 348 + 384 + 834 + 843 + 438 + 483  = 3675
+# If the number has a digit 0, the numbers formed by a leading 0 should be discarded:
+#
+# sc_perm_comb(340) = 3 + 4 + 34 + 30 + 40 + 43 + 340 + 304 + 430 + 403 = 1631
+# Duplicate permutations can occur if the number has more than once the same digit, but they should be added only once in the result:
+#
+# sc_perm_comb(333) = 3 + 33 + 333 = 369
+# If the number has only one digit its score is the same number:
+#
+# sc_perm_comb(6) = 6
+# sc_perm_comb(0) = 0
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+def sc_perm_comb(n)
+  return n if n.to_s.size == 1
+  arr = n.digits
+  (1..arr.size).map{|k| arr.permutation(k).map{|a| a.join.to_i}}.flatten.uniq.sum
+end
