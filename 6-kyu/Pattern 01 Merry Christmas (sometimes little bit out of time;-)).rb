41044cfa616ad138b748920170f20a58670bff6a
krillan <pushkinidze49@gmail.com>
2023-12-07 17:38:17 +0300

    6-kyu/Pattern 01 Merry Christmas (sometimes little bit out of time;-))

---
+++ 6-kyu/Pattern 01 Merry Christmas (sometimes little bit out of time;-)).rb
@@ -0,0 +1,67 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# Here your task is to Create a (nice) Christmas Tree.
+#
+# You don't have to check errors or incorrect input values, every thing is ok without bad tricks, only one int parameter as input and a string to return;-)...
+#
+# So what to do?
+#
+# First three easy examples:
+#
+# Input: 3 and Output:
+#   *
+#  ***
+# *****
+#  ###
+#
+#  Input 9 and Output:
+#     *
+#    ***
+#   *****
+#    ***
+#   *****
+#  *******
+#   *****
+#  *******
+# *********
+#    ###
+#
+#  Input 17 and Output:
+#       *
+#      ***
+#     *****
+#      ***
+#     *****
+#    *******
+#     *****
+#    *******
+#   *********
+#    *******
+#   *********
+#  ***********
+#   *********
+#  ***********
+# *************
+#      ###
+#
+# Really nice trees, or what???! So merry Christmas;-)
+# You can see, always a root, always steps of hight 3, tree never smaller than 3 (return "") and no difference for input values like 15 or 17 (because (int) 15/3 = (int) 17/3). That's valid for every input and every tree. Lines are delimited by \r\n and no trailing white space allowed. I think there's nothing more to say - perhaps look at the testcases too;-)!
+#
+# There are some static tests at the beginning and many random tests if you submit your solution.
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+def christmas_tree(height)
+  return '' if height < 3
+  h = height / 3
+  l = 5 + (h - 1) * 2
+  t = Array.new(h + 2){ Array.new(l, '*') }
+           .map.with_index{|a, i| a.map.with_index{|e, j| j < i || j > a.size - 1 - i ? ' ' : e}}
+           .map{|a| a.join.rstrip}
+           .reverse
+  ((1..h).map.with_index{|a, i| t[i..i+2]}.flatten + [' ' * ((l - 3) / 2) + '###']).join("\r\n")
+end
