1eea41aeca8d76bee0f5b7d0e1d76347a80874d3
krillan <pushkinidze49@gmail.com>
2024-01-11 17:52:12 +0300

    6-kyu/Simple Fun #161 Replace Dashes As One

---
+++ 6-kyu/Simple Fun #161 Replace Dashes As One.rb
@@ -0,0 +1,25 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# If string has more than one neighboring dashes(e.g. --) replace they with one dash(-).
+#
+# Dashes are considered neighbors even if there is some whitespace between them.
+#
+# Example
+# For str = "we-are- - - code----warriors.-"
+#
+# The result should be "we-are- code-warriors.-"
+#
+# Input/Output
+# [input] string str
+#
+# [output] a string
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+def replace_dashes_as_one(s)
+  s.gsub(/-+ +-|-+-/, '-').gsub(/-+ +-|-+-/, '-').gsub(/-+ +-|-+-/, '-')
+end
