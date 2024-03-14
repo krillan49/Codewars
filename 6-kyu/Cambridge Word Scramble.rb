4c504ba63eec8249be210dd38752bf275d343fb4
krillan <pushkinidze49@gmail.com>
2024-03-14 23:05:55 +0300

    6-kyu/Cambridge Word Scramble

---
+++ 6-kyu/Cambridge Word Scramble.rb
@@ -0,0 +1,39 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# One of the first chain emails I ever received was about a supposed Cambridge University study that suggests your brain can read words no matter what order the letters are in, as long as the first and last letters of each word are correct.
+#
+# Your task is to create a function that can take any string and randomly jumble the letters within each word while leaving the first and last letters of the word in place.
+#
+# For example,
+#
+# mixwords('Winter is coming') // returns 'Wntier is cminog' or 'Wtiner is conimg'
+# mixwords('Hey, friends!') // returns 'Hey, fierdns!' or 'Hey, fernids!'
+# All punctuation should remain in place
+# Words smaller than 3 letters should not change
+# Letters must be randomly moved (and so calling the function multiple times with the same string should return different values
+# Parameters that are not strings should return undefined
+# The tests do the following things to ensure a valid string is returned:
+#
+# Check that the string returned is not equal to the string passed (you may have to revalidate the solution if your function randomly returns the same string)
+# Check that first and last letters of words remain in place
+# Check that punctuation remains in place
+# Checks string length remains the same
+# Checks the function returns undefined for non-strings
+# Checks that word interiors (the letters between the first and last) maintain the same letters, albeit in a different order
+# Checks that letters are randomly assigned.
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+def mix_words(str)
+  return 'undefined' if str.class != String
+  a = str.chars.slice_when{|a, b| (/[a-z]/i === a && /[^a-z]/i === b) || (/[a-z]/i === b && /[^a-z]/i === a)}.to_a
+  return str if a.all?{|ar| ar.size < 4 || ar.all?{|c| /[^a-z]/i === c}}
+  loop do
+    a = a.map{|a| (a.size > 3 && a.all?{|c| /[a-z]/i === c}) ? [a[0]] + a[1..-2].shuffle! + [a[-1]] : a}
+    return a.join if a.join != str
+  end
+end
