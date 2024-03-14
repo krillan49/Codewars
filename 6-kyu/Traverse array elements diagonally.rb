d29ec636712bd1256039a9f6864b7ba4a46ef971
krillan <pushkinidze49@gmail.com>
2024-03-14 23:01:31 +0300

    6-kyu/Traverse array elements diagonally

---
+++ 6-kyu/Traverse array elements diagonally.rb
@@ -0,0 +1,44 @@
+#===============================================================================================================
+#                                               Description
+#===============================================================================================================
+
+# In this kata you're given an n x n array and you're expected to traverse the elements diagonally from the bottom right to the top left.
+#
+# Example
+#   1 6 7
+#   7 2 4
+#   3 5 9
+# your solution should return elements in the following order
+#
+# 9
+# 4 5
+# 7 2 3
+# 6 7
+# 1
+# //=> [9, 4, 5, 7, 2, 3, 6, 7, 1]
+#
+# Your task is to write the function diagonal() that returns the array elements in the above manner.
+#
+# Another Example
+# arr = [
+#  [4, 5, 7],
+#  [3, 9, 1],
+#  [7, 6, 2]
+# ]
+#
+# diagonal(arr) //=> [2, 1, 6, 7, 9, 7, 5, 3, 4]
+# You can assume the test cases are well formed.
+
+#===============================================================================================================
+#                                               Solution
+#===============================================================================================================
+
+def diagonal(arr)
+  arr.transpose
+     .map(&:reverse)
+     .reverse
+     .map.with_index{|a, i| a.map.with_index{|e, j| [e, i, j]}}
+     .flatten(1)
+     .sort_by{|a| [a[1] + a[2], a[1]]}
+     .map(&:first)
+end
